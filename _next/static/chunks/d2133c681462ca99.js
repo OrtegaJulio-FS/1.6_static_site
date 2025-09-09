89b012a414b2a8a0cefcebd47962e5464bd2140d
OrtegaJulio-FS <jcortega1@student.fullsail.edu>
2025-09-08 22:25:57 -0500

    updates for better style

---
+++ _next/static/chunks/d2133c681462ca99.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/61be62737cc2bbb8.js",
+  "static/chunks/turbopack-f8bc69f416d46155.js"
+])
--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/1.6_static_site",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
